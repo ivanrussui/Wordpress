c02c02c22a2c59687fdaa2199f82cd9bd9886c71
Ivan <fenomen_ny@mail.ru>
2021-03-26 01:42:09 +0300

    animation+form

---
+++ dist/js/script.js
@@ -0,0 +1,73 @@
+$(document).ready(function () {
+
+	// modal
+	$('[data-name=modal]').on('click', function() {
+		$('.overlay, #modal').fadeIn(500);
+		$('body').toggleClass('lock');
+	});
+
+	$('.modal__close').on('click', function() {
+		$('.overlay, #modal, #thanks').fadeOut(500);
+		$('body').toggleClass('lock');
+	});
+
+	// validate form
+	$('#form').validate({
+		rules: {
+			name:	{
+				required: true,
+				minlength: 2,
+			},
+			phone: "required",
+			email: {
+				required: true,
+				email: true,
+			},
+		},
+		messages: {
+			name: {
+				required: 'Пожалуйста, введите своё имя',
+				minlength: jQuery.validator.format('Введите от {0} букв'),
+			},
+			phone: 'Пожалуйста, введите свой номер телефона',
+			email: {
+				required: 'Пожалуйста, введите свою почту',
+        email: 'Неправильно введена почта', 
+			},
+		},
+	});
+
+  // Masked Input
+  $('input[name=phone').mask('+7 (999) 999-9999');
+
+	// ajax
+	$('#form').submit(function(e) {
+		e.preventDefault();
+
+		// это условие внутри Ajax чтобы письмо пустым не отправлялось
+		if (!$(this).valid()) {
+			return;
+		}
+
+		$.ajax({
+			type: 'POST',
+			url: 'mailer/smart.php',
+			data: $(this).serialize(),
+		}).done(function() {
+			$(this).find('input').val('');
+			$('#modal').fadeOut(500);
+			$('#thanks').fadeIn(500);
+
+
+			$('form').trigger('reset');
+		});
+		return false;
+	});
+	
+	$('.modal__close').on('click', function() {
+		$('.overlay, #thanks').fadeOut(500);
+	});
+
+
+
+});
