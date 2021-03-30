3ed9a50e79f8eb1bfd44b1ae20d8ec894abf597a
Ivan <fenomen_ny@mail.ru>
2021-03-30 11:50:36 +0300

    pageup

--- src/js/script.js
+++ src/js/script.js
@@ -68,6 +68,20 @@ $(document).ready(function () {
 		$('.overlay, #thanks').fadeOut(500);
 	});
 
+	// кнопка наверх
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 700) {
+      $('.pageup').slideDown();
+    } else {
+      $('.pageup').slideUp();
+    }
+  });
 
+  // плавность перехода по всем ссылкам на сайте
+  $("a[href^='#']").click(function () {
+    const _href = $(this).attr('href');
+    $('html, body').animate({ scrollTop: $(_href).offset().top + 'px' });
+    return false;
+  });
 
 });
